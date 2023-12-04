b305c0420bce1341077bebb9b9ff58a48f75c965
Anand Panda <anand612p@gmail.com>
2023-12-05 00:28:34 +0530

    added

--- 013/140-heap.cpp
+++ 013/140-heap.cpp
@@ -53,40 +53,54 @@ public:
     }
 };
 
-void heapify(int arr[], int n, bool mH = false){
-    static int in = n/2;
+void heapify(vector<int> &a, int size, int in, bool mH = false){
     if(!in) return; 
-    int li = in, ri = in--, f = 0;
-    while(!f && li <= n/2 
-    && ri <= n/2){
+
+    int n = size, i = in, f = 0;
+    while(!f){
         f = 1;
-        if((mH? arr[li] > arr[li*2] : arr[li] < arr[li*2])){
-            swap(arr[li], arr[li*2]);
-            li *= 2, f = 0;
-        }
-        if(2*ri+1 <= n && (mH? arr[ri] > arr[ri*2+1] : arr[ri] < arr[ri*2+1])){
-            swap(arr[ri], arr[ri*2+1]);
-            ri = ri*2+1, f = 0;
+        if(2*i <= n){
+            int x = (2*i+1 <= n);
+            int y = x && (mH? a[2*i] > a[i*2+1] : a[2*i] < a[i*2+1]);
+            if((mH? a[i] > a[2*i+y] : a[i] < a[2*i+y])){
+                swap(a[i], a[2*i+y]);
+                i = 2*i + y, f = 0;
+            }
         }
     }
-    heapify(arr, n, mH);
+    heapify(a, size, in-1, mH);
+}
+
+void helpHeapSort(vector<int> &a, int in, bool mH = false){
+    if(in == 1) return;
+    swap(a[1], a[in]);
+    heapify(a, in-1, 1, mH);
+    helpHeapSort(a, in-1, mH);
+}
+void heapSort(vector<int> &a, int size, bool mH = false){
+    heapify(a, size, size/2, mH);
+    helpHeapSort(a, size, mH);
 }
 
 int main(){
     
-    heap h; // max heap
+    // heap h; // max heap
     // heap h(1) // min heap
-    h.insert(50);
-    h.insert(55);
-    h.insert(53);
-    h.insert(52);
-    h.insert(54); // 5
+    // h.insert(50);
+    // h.insert(55);
+    // h.insert(53);
+    // h.insert(52);
+    // h.insert(54); // 5
 
-    h.del();
-    // h.print();
+    // h.del();
+    // h.pint();
 
-    int arr[] = {-1, 50, 55, 53, 52, -2, 54};
-    heapify(arr, 6);
-    for(int i=1; i<=6; i++) cout << arr[i] << " ";
+    vector<int> a = {-1, 50, 62, 53, 55, -2, 54};
+    int n = a.size()-1;
+    // heapify(a, n, n/2, 1);
+
+    heapSort(a, n);
+    for(int i=1; i<=6; i++) cout << a[i] << " ";
+
     return 0;
 }
